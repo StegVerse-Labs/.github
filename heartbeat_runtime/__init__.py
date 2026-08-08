d15d9c14aefe8c998b8887b2a396586c27ca5d8a
StegVerse <210078727+StegVerse@users.noreply.github.com>
2026-08-08 00:45:24 -0500

    feat: activate policy-continuity runtime v7

--- heartbeat_runtime/__init__.py
+++ heartbeat_runtime/__init__.py
@@ -1,6 +1,6 @@
 """StegVerse single-heartbeat runtime primitives."""
 
-from .engine_v6 import HeartbeatRuntime, WorkerResponse
+from .engine_v7 import HeartbeatRuntime, WorkerResponse
 from .process_adapter import ProcessWorkerAdapter
 
 __all__ = ["HeartbeatRuntime", "WorkerResponse", "ProcessWorkerAdapter"]
