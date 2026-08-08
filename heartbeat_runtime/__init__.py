d3d08fd9b9a78a6c3f8514840ae3329cfbed03c9
StegVerse <210078727+StegVerse@users.noreply.github.com>
2026-08-07 22:38:06 -0500

    feat: export sole epoch-owning heartbeat runtime

--- heartbeat_runtime/__init__.py
+++ heartbeat_runtime/__init__.py
@@ -1,6 +1,6 @@
 """StegVerse single-heartbeat runtime primitives."""
 
-from .engine_v2 import HeartbeatRuntime, WorkerResponse
+from .engine_v3 import HeartbeatRuntime, WorkerResponse
 from .process_adapter import ProcessWorkerAdapter
 
 __all__ = ["HeartbeatRuntime", "WorkerResponse", "ProcessWorkerAdapter"]
