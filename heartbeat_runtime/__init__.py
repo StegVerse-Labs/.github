f003874397e2a3cd94386afa279db52ede9a7e3e
StegVerse <210078727+StegVerse@users.noreply.github.com>
2026-08-08 00:24:22 -0500

    feat: activate canonical goal-lineage runtime v5

--- heartbeat_runtime/__init__.py
+++ heartbeat_runtime/__init__.py
@@ -1,6 +1,6 @@
 """StegVerse single-heartbeat runtime primitives."""
 
-from .engine_v4 import HeartbeatRuntime, WorkerResponse
+from .engine_v5 import HeartbeatRuntime, WorkerResponse
 from .process_adapter import ProcessWorkerAdapter
 
 __all__ = ["HeartbeatRuntime", "WorkerResponse", "ProcessWorkerAdapter"]
